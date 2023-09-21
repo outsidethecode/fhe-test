6cacabd5c981b4ec952a23b4d476d4e36b1fb579
outsidethecode <outsidethecode@gmail.com>
2023-09-21 13:09:08 +0100

    first working version

--- src/main.rs
+++ src/main.rs
@@ -25,34 +25,32 @@ fn to_chunks(number: u64, width: u32, chunk_size: u32) -> Vec<u64> {
     chunks
 }
 
-fn u8_array_to_u32_array(arr: &[u8; 32]) -> [u32; 8] {
-    let mut result: [u32; 8] = [0; 8];
+fn u8_32_array_to_u16_16_array(arr: &[u8; 32]) -> [u16; 16] {
+    let mut result: [u16; 16] = [0; 16];
 
-    for i in 0..8 {
-        let sub_arr = &arr[i*4 .. (i+1)*4];
-        result[i] = calculate_value2(sub_arr);
+    for i in 0..16 {
+        let sub_arr = &arr[i*2 .. (i+1)*2];
+        result[i] = calc_slice_value(sub_arr);
     }
 
     result
 }
-
-
-// fn u8_array_to_u32_array2(arr: &[u8; 8]) -> [u32; 2] {
-//     let mut result: [u32; 2] = [0; 2];
-
-//     for i in 0..2 {
-//         result[i] = calculate_value2(&arr[i*4 .. (i+1)*4]);
-//     }
-
-//     result
-// }
-
 
 fn calculate_value(byte_array: &[u8; 32]) -> BigUint {
     let mut value: BigUint = BigUint::from(0u128);
     
     for &byte in byte_array.iter() {
         value = (value << 8) | BigUint::from(byte);
+    }
+    
+    value
+}
+
+fn calc_slice_value(byte_array: &[u8]) -> u16 {
+    let mut value: u16 = 0u16;
+    
+    for &byte in byte_array.iter() {
+        value = (value << 8) | u16::from(byte);
     }
     
     value
@@ -78,10 +76,10 @@ fn calculate_value3(byte_array: &[u8]) -> u32 {
     value
 }
 
-fn calc_value(array: [u32; 8]) -> BigUint {
-    let mut value: BigUint = BigUint::from(0u32);
-    for i in (0..8) {
-        value <<= 32;
+fn calc_value(array: [u16; 16]) -> BigUint {
+    let mut value: BigUint = BigUint::from(0u16);
+    for i in (0..16) {
+        value <<= 16;
         value += BigUint::from(array[i]);
     }
     
@@ -133,7 +131,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             | (ex1[start_index + 3] as u32);
     }
 
-    let compressed_ex1 = u8_array_to_u32_array(&ex1);
+    let compressed_ex1 = u8_32_array_to_u16_16_array(&ex1);
     print!("compressed_ex1 : 1 {:?}\n", compressed_ex1);
     print!("compressed_ex1 : 2 {:?}\n", u32_array);
 
@@ -168,12 +166,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     set_server_key(server_keys);
 
 
-    let aaa = FheUint32::try_encrypt(65535u16, &public_key).unwrap();
-    let bbb = FheUint32::try_encrypt(65535u16, &public_key).unwrap();
-    let ccc = &aaa + &bbb;
-    let ddd: u32 = ccc.decrypt(&client_key);
+    // let aaa = FheUint32::try_encrypt(65535u16, &public_key).unwrap();
+    // let bbb = FheUint32::try_encrypt(65535u16, &public_key).unwrap();
+    // let ccc = &aaa + &bbb;
+    // let ddd: u32 = ccc.decrypt(&client_key);
 
-    println!("DDDDDDDDDDDDDDDDDDDDDDDDD: {}", ddd);
+    // println!("DDDDDDDDDDDDDDDDDDDDDDDDD: {}", ddd);
 
 
     let mut c1 = Vec::new();
@@ -183,7 +181,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     c1.push(xxx);
     c1.remove(0);
 
-    let compressed_sk1 = u8_array_to_u32_array(&ex1);
+    let compressed_sk1 = u8_32_array_to_u16_16_array(&ex1);
     for (i, chunk) in compressed_sk1.iter().enumerate() {
         println!("Chunk {}: {}", i, chunk);
         c1.push(FheUint32::try_encrypt(*chunk, &public_key).unwrap());
@@ -193,7 +191,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     c2.push(yyy);
     c2.remove(0);
 
-    let compressed_sk2 = u8_array_to_u32_array(&ex2);
+    let compressed_sk2 = u8_32_array_to_u16_16_array(&ex2);
     for (i, chunk) in compressed_sk2.iter().enumerate() {
         println!("Chunk {}: {}", i, chunk);
         c2.push(FheUint32::try_encrypt(*chunk, &public_key).unwrap());
@@ -202,7 +200,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start2 = Instant::now();
 
     let mut c = Vec::new();
-    for i in 0..8 {
+    for i in 0..16 {
         c.push(&c1[i] + &c2[i]);
     }
 
@@ -210,41 +208,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Time elapsed for calculating C () is: {:?}", duration2);
 
   
+    let mut value: BigUint = BigUint::from(0u16);
     for element in c.iter() {
         let decrypted_ci : u64 = element.decrypt(&client_key);
         println!("Deeecrpted i {}", decrypted_ci);
-
+        value <<= 16;
+        value += BigUint::from(decrypted_ci);
 
     }
 
-    let np1: u32 = c[7].decrypt(&client_key);
-    println!("np1 = {}", np1);
-    let np2: u32 = c[6].decrypt(&client_key);
-    println!("np2 = {}", np2);
-    let np3: u32 = c[5].decrypt(&client_key);
-    println!("np3 = {}", np3);
-    let np4: u32 = c[4].decrypt(&client_key);
-    println!("np4 = {}", np4);
-    let np5: u32 = c[3].decrypt(&client_key);
-    println!("np5 = {}", np5);
-    let np6: u32 = c[2].decrypt(&client_key);
-    println!("np6 = {}", np6);
-    let np7: u32 = c[1].decrypt(&client_key);
-    println!("np7 = {}", np7);
-    let np8: u32 = c[0].decrypt(&client_key);
-    println!("np8 = {}", np8);
+    // let np1: u32 = c[7].decrypt(&client_key);
+    // println!("np1 = {}", np1);
+    // let np2: u32 = c[6].decrypt(&client_key);
+    // println!("np2 = {}", np2);
+    // let np3: u32 = c[5].decrypt(&client_key);
+    // println!("np3 = {}", np3);
+    // let np4: u32 = c[4].decrypt(&client_key);
+    // println!("np4 = {}", np4);
+    // let np5: u32 = c[3].decrypt(&client_key);
+    // println!("np5 = {}", np5);
+    // let np6: u32 = c[2].decrypt(&client_key);
+    // println!("np6 = {}", np6);
+    // let np7: u32 = c[1].decrypt(&client_key);
+    // println!("np7 = {}", np7);
+    // let np8: u32 = c[0].decrypt(&client_key);
+    // println!("np8 = {}", np8);
 
-    // Combine the results to get the final sum
-    let result = (BigUint::from(np1) << 224)
-        | (BigUint::from(np2) << 192)
-        | (BigUint::from(np3) << 160)
-        | (BigUint::from(np4) << 128)
-        | (BigUint::from(np5) << 96)
-        | (BigUint::from(np6) << 64)
-        | (BigUint::from(np7) << 32)
-        | BigUint::from(np8);
+    // // Combine the results to get the final sum
+    // let result = (BigUint::from(np1) << 224)
+    //     | (BigUint::from(np2) << 192)
+    //     | (BigUint::from(np3) << 160)
+    //     | (BigUint::from(np4) << 128)
+    //     | (BigUint::from(np5) << 96)
+    //     | (BigUint::from(np6) << 64)
+    //     | (BigUint::from(np7) << 32)
+    //     | BigUint::from(np8);
 
-    println!("Result: {:?}", result);
+
+    println!("Result: {:?}", value);
 
     // let compressed_x1_value = calc_value(c);
     // print!("ex1 value: 3 {:?}\n", compressed_x1_value.to_string());
